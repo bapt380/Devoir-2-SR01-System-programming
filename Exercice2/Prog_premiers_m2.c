c59f84e529f9e695c5cbf52b168a54ba05fee025
bapt380 <baptvlsup@gmail.com>
2020-12-19 11:19:34 +0100

    construction du Prog_premiers_m2.c réussie !

--- Exercice2/Prog_premiers_m2.c
+++ Exercice2/Prog_premiers_m2.c
@@ -1,3 +1,10 @@
+/*
+Donner  une  autre  version  de  ce  programme 
+(nommez-le Prog_premiers_m2.c)en utilisant  
+excev()pour  implémenter  une  fonction  my_system(char  *)  
+qui  remplace system().
+*/
+
 #include<unistd.h>
 
 #include<stdio.h>
@@ -17,9 +24,16 @@ int premier(int nb) {
     else return 1; // on a seulement 1 et nb comme diviseur donc premier
 }
 
+my_system(char  *argv)
+{
+    execv("/bin/echo",(char **const)argv);
+}
+
 void explorer(int debut, int fin) {
     int etat, pid, pid2;
     pid = fork(); // creer pere / fils
+
+    //char *argv[3];
 
     if (pid == 0) { // on est dans le fils
 
@@ -28,16 +42,40 @@ void explorer(int debut, int fin) {
             if (premier(i) == 1) { // si l'indice est premier
                 
                 pid2 = fork(); // creer fils pere
-                
+                FILE * fa;
                 if (pid2 == 0) { // on rentre dans le fils
                     
                     char chaine[100];
-                    sprintf(chaine, "echo '%d  est un nombre premier écrit par le processus %d'>>nbr_premiers.txt", i, getpid());
+                    char chaine2[100];
+
+                    sprintf(chaine, "%d  est un nombre premier écrit par le processus %d", i, getpid());
                     // sprintf stocke la chaîne formatée en mémoire dans un buffer pré-alloué nommée chaine.
-                    // echo associé au >> (redirection) va permettre d'ajouter à la fin du fichier nbr_premier.txt la chaine de caractères entre single quote au lieu de l'afficher sur l'écran.
-                    system(chaine); // renvoie 0 si tout s'est bien passé, exécute la commande "chaine"
-                    // c'est cette instruction qui va permettre l'écriture dans le fichier
+
+                    // TODO Question à se poser : pourquoi avec cette méthode la syncronisation a bien eu lieu ??
+                    // EXEMPLE : on voit que les deux pid se suivent
+                    // 7  est un nombre premier écrit par le processus 9020
+                    // 11  est un nombre premier écrit par le processus 9021
+
+                    fa = freopen("nbr_premiers.txt","a+",stdout);
+                    // duplication et remplacement en meme temps
+                    // remplacement du stdout par le fichier 
+                    // le stdout est fermé et remplacé par le fichier
+  
+                    if (fa==NULL) {
+                        perror("freop erreur :");
+                        exit(0);
+                    }
                     
+                    /*argv[0] = "echo";
+                    argv[1] = chaine;
+                    argv[2] = NULL;*/
+                    char *argv[4] = {"echo",chaine,NULL};
+
+                    my_system(argv);
+                    fclose(fa); // fermer le fichier pas le stdout
+                    fa = freopen("/dev/tty","a+",stdout); // on remet dans le stdout sa valeur de base 
+
+
                     sleep(2); // permet d'aller dans le père (l'instruction wait dans le père sera ainsi exécutée qui va entrainer un retour dans le fils)
                     exit(0); // terminaison normale du fils
                 } 
