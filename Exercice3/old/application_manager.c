6324a1ba08007a7c2b72f22ebb0102b211c575d1
jonathan <johnlegrand972@gmail.com>
2020-12-19 18:57:15 -0400

    Correction des bugs dans la gestion des processus

--- Exercice3/old/application_manager.c
+++ Exercice3/old/application_manager.c
@@ -10,7 +10,7 @@
  *       Revision:  none
  *       Compiler:  gcc
  *
- *         Author:  YOUR NAME (), 
+ *         Author:  Jonathan Legrand 
  *   Organization:  
  *
  * =====================================================================================
@@ -21,22 +21,121 @@
 #include <errno.h>
 #include <sys/wait.h>
 
+#define READ 0
+#define WRITE 1
+
+
 
 /* 
  * ===  FUNCTION  ======================================================================
- *         Name:  lauch_get_time
+ *         Name:  wait_process
  *  Description:  
  * =====================================================================================
  */
     void
-launch_get_time (char **arg_get_time)
+wait_process ( char* process_name, pid_t pid )
 {
-    if (execv("./get_time", arg_get_time) == -1){
-        perror("execv ");
-        exit(EXIT_FAILURE);
+    int status;
+    waitpid(pid,&status,0);
+    if(WIFEXITED(status)){
+        printf("Terminaison normale de %s avec le code %d\n",process_name,WEXITSTATUS(status));
+    } 
+    if(WIFSIGNALED(status)){
+        printf("Le processus %s a été terminé par le signal %d\n",process_name,WTERMSIG(status));
     }
-}		/* -----  end of function lauch_get_time  ----- */
 
+
+}		/* -----  end of function process_result  ----- */
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  launch_application
+ *  Description:  
+ * =====================================================================================
+ */
+    void
+launch_application ( char *path, char **arg )
+{
+    if(execv(path,arg)== -1){
+        perror("execv");
+            exit(EXIT_FAILURE);
+    }
+}		/* -----  end of function launch_network_manager  ----- */
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  case_get_time
+ *  Description:  
+ * =====================================================================================
+ */
+    void
+case_get_time (pid_t get_time_id)
+{
+    int *pid_son;
+    char *arg_get_time[] = {"get_time", NULL};
+    switch(get_time_id){
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+            break;
+
+        case 0:
+            *pid_son = getpid();
+            printf("pid get_time = %d\n",*pid_son);
+            launch_application("./bin/get_time",arg_get_time);
+
+            break;
+
+        default:
+            wait_process("get_time",*pid_son);
+            break;
+    }
+
+}		/* -----  end of function case_get_time  ----- */
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  case_network_manager
+ *  Description:  
+ * =====================================================================================
+ */
+    void
+case_network_manager (pid_t network_manager_id)
+{
+    int *pid_son;
+    int pid;
+    int fd[2];
+    pipe(fd);
+    char *arg_network_manager[] = {"network_manager", NULL};
+    switch(network_manager_id){
+        case -1:
+            perror("fork");
+            exit(EXIT_FAILURE);
+            break;
+
+        case 0:
+            *pid_son = getpid();
+            close(fd[READ]);
+            if ((write(fd[WRITE],pid_son,sizeof(pid_son))) == -1){
+                fprintf(stderr,"Erreur lors de l'écriture dans le pipe\n");
+            }
+            close(fd[WRITE]);
+            printf("pid network_manager = %d\n",*pid_son);
+            launch_application("./bin/network_manager",arg_network_manager);
+
+            break;
+
+        default:
+            
+            close(fd[WRITE]);
+            read(fd[READ],&pid,sizeof(pid_son));
+            printf("Reçu par le pipe : %d\n",pid);
+            wait_process("network_manager",*pid_son);
+            break;
+    }
+
+}		/* -----  end of function case_network_id  ----- */
 
 /* 
  * ===  FUNCTION  ======================================================================
@@ -48,25 +147,24 @@ launch_get_time (char **arg_get_time)
 main ( int argc, char *argv[] )
 
 {
-   pid_t get_time_id = fork();
-   char *arg_get_time[] = {"get_time", NULL};
-   /*if (get_time_id != 0){
-       pid_t network_manager_id = fork();
-   }*/
-  switch(get_time_id){
-      case -1:
-          perror("fork ");
-          exit(EXIT_FAILURE);
-          break;
 
-      case 0:
-          launch_get_time(arg_get_time);
-          break;
 
-      default:
-          wait(NULL);
-          break;
-  }
+    pid_t network_manager_id = -1;
+    pid_t get_time_id = fork();
+    if (get_time_id != 0){
+        network_manager_id = fork();
+        case_network_manager(network_manager_id);
+    }
+#if 0
+    /*On s'assure que le processus qui rentre dans case_network_manager
+     * est soit le père, soit le processus network_manager*/
+    if(network_manager_id != -1 || get_time_id != 0 ){
+        printf("pid avant le case network %d\n",getpid());
+        case_network_manager(network_manager_id);
+        return EXIT_SUCCESS;
+    } 
+#endif
+    case_get_time(get_time_id);
 
 
     return EXIT_SUCCESS;
--- Exercice3/src/application_manager.c
+++ Exercice3/src/application_manager.c
@@ -21,6 +21,9 @@
 #include <errno.h>
 #include <sys/wait.h>
 
+#define READ 0
+#define WRITE 1
+
 
 
 /* 
@@ -33,9 +36,12 @@
 wait_process ( char* process_name )
 {
     int status;
-    wait(&status);
+    waitpid(-1,&status,0);
     if(WIFEXITED(status)){
         printf("Terminaison normale de %s avec le code %d\n",process_name,WEXITSTATUS(status));
+    } 
+    if(WIFSIGNALED(status)){
+        printf("Le processus %s a été terminé par le signal %d\n",process_name,WTERMSIG(status));
     }
 
 
@@ -63,7 +69,7 @@ launch_application ( char *path, char **arg )
  * =====================================================================================
  */
     void
-case_get_time (pid_t get_time_id)
+case_get_time (pid_t get_time_id,pid_t father_id)
 {
     char *arg_get_time[] = {"get_time", NULL};
     switch(get_time_id){
@@ -73,12 +79,15 @@ case_get_time (pid_t get_time_id)
             break;
 
         case 0:
+            printf("lauching get_time with pid:%d\n",getpid());
             launch_application("./bin/get_time",arg_get_time);
 
             break;
 
         default:
+            printf("father process: waiting for get_time...\n");
             wait_process("get_time");
+
             break;
     }
 
@@ -92,8 +101,10 @@ case_get_time (pid_t get_time_id)
  * =====================================================================================
  */
     void
-case_network_manager (pid_t network_manager_id)
+case_network_manager (pid_t network_manager_id, pid_t father_id)
 {
+    printf("In case_network_manager: father_id:%d\n",father_id);
+    printf("In case_network_manager: id:%d\n",network_manager_id);
     char *arg_network_manager[] = {"network_manager", NULL};
     switch(network_manager_id){
         case -1:
@@ -102,11 +113,14 @@ case_network_manager (pid_t network_manager_id)
             break;
 
         case 0:
+            printf("lauching network_manager with pid:%d\n",getpid());
+            sleep(2);
             launch_application("./bin/network_manager",arg_network_manager);
 
             break;
 
         default:
+            printf("father process: waiting for network_manager...\n");
             wait_process("network_manager");
             break;
     }
@@ -123,15 +137,26 @@ case_network_manager (pid_t network_manager_id)
 main ( int argc, char *argv[] )
 
 {
-   
 
+
+    int father_id = getpid();
+    pid_t network_manager_id = -1;
     pid_t get_time_id = fork();
-    if (get_time_id != 0){
-      pid_t network_manager_id = fork();
-      case_network_manager(network_manager_id);
-      }
-    case_get_time(get_time_id);
+    if (get_time_id != 0) network_manager_id = fork();
     
+#if 0
+    /*On s'assure que le processus qui rentre dans case_network_manager
+     * est soit le père, soit le processus network_manager*/
+    if(network_manager_id != -1 || get_time_id != 0 ){
+        printf("pid avant le case network %d\n",getpid());
+        case_network_manager(network_manager_id);
+        return EXIT_SUCCESS;
+    } 
+#endif
+    printf("father_id:%d\n",(int)father_id);
+    if(get_time_id == 0 || network_manager_id != 0) case_get_time(get_time_id,father_id);
+    sleep(2);
+    if(get_time_id != 0 || network_manager_id == 0) case_network_manager(network_manager_id,father_id);
 
     return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
