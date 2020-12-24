8e22ee3e5202554c4a70a3bcc7a6b6de190e8d17
jonathan <johnlegrand972@gmail.com>
2020-12-24 12:17:03 -0400

    Intégration de power_manager dans application_manager et dans le makefile

--- Exercice3/src/application_manager.c
+++ Exercice3/src/application_manager.c
@@ -20,6 +20,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <assert.h>
 
 
 /* 
@@ -70,17 +71,12 @@ case_get_time (pid_t get_time_id)
 {
     char *arg_get_time[] = {"get_time", NULL};
     switch(get_time_id){
-        case -1:
-            perror("fork");
-            exit(EXIT_FAILURE);
-            break;
-
         case 0:
             launch_application("./bin/get_time",arg_get_time);
             break;
 
         default:
-            printf("[application_manger] father process: waiting for get_time...\n");
+            printf("[application_manager] father process: waiting for get_time...\n");
             wait_process("get_time");
 
             break;
@@ -100,11 +96,6 @@ case_network_manager (pid_t network_manager_id)
 {
     char *arg_network_manager[] = {"network_manager", NULL};
     switch(network_manager_id){
-        case -1:
-            perror("fork");
-            exit(EXIT_FAILURE);
-            break;
-
         case 0:
             launch_application("./bin/network_manager",arg_network_manager);
             break;
@@ -119,6 +110,26 @@ case_network_manager (pid_t network_manager_id)
 
 }		/* -----  end of function case_network_id  ----- */
 
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  case_power_manager
+ *  Description:  
+ * =====================================================================================
+ */
+    void
+case_power_manager ( pid_t power_manager_id )
+{
+    char *arg_power_manager[] = {"power_manager", "./mise_en_veille.txt", "4", NULL};
+    if (power_manager_id != 0){
+        printf("[application manager] father process: waiting for power_manager...\n");
+        wait_process("power_manager");
+    } else {
+        launch_application("./bin/power_manager",arg_power_manager);
+    }
+
+}		/* -----  end of function case_power_manager  ----- */
+
 /* 
  * ===  FUNCTION  ======================================================================
  *         Name:  application manager
@@ -129,17 +140,35 @@ case_network_manager (pid_t network_manager_id)
 main ( int argc, char *argv[] )
 
 {
-    pid_t network_manager_id = -1;
-    pid_t get_time_id = fork();
-    if (get_time_id != 0) network_manager_id = fork();
+    /* Déclaration des pid */
+    pid_t get_time_id;
+    pid_t network_manager_id;
+    pid_t power_manager_id;
+    pid_t father_id = getpid();
+
+    /*  Création des processus fils */
+    get_time_id = fork();
+    assert(get_time_id != -1);
+
+    if (getpid() == father_id) network_manager_id = fork();
     
+    assert(network_manager_id != -1);
+
+    if (getpid() == father_id) power_manager_id = fork();
+    assert(power_manager_id != -1);
+
+
     /*On s'assure que le processus qui rentre dans case_X
      * est soit le père, soit le processus X*/
-    if(get_time_id == 0 || network_manager_id != 0) case_get_time(get_time_id);
-#if 0 
-    sleep(2);
-#endif
-    if(get_time_id != 0 || network_manager_id == 0) case_network_manager(network_manager_id);
+    if(get_time_id == 0 
+            || getpid() == father_id ) case_get_time(get_time_id);
+
+    if(network_manager_id == 0 
+            || getpid() == father_id ) case_network_manager(network_manager_id);
+
+    if(power_manager_id == 0 
+            || getpid() == father_id ) case_power_manager(power_manager_id);
+
 
     return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
