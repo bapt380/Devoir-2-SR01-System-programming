ef88b223cea84e6edf8b7d681c8c702d2dd1caa5
bapt380 <baptvlsup@gmail.com>
2020-12-24 12:56:05 +0100

    programs m1,m2,m3 work ? I hope so :)

--- Exercice2/Prog_premiers_m1.c
+++ Exercice2/Prog_premiers_m1.c
@@ -8,6 +8,10 @@
 
 #include<string.h>
 
+/*
+1) Écrire le  pid  du  processus  qui  a créé le  processus system et  le  père  de  ce processus (processus qui exécute la boucle for)
+2) Écrire le pid du processus qui a écrit le message sur le terminal
+*/
 
 
 int premier(int nb) {
@@ -18,39 +22,6 @@ int premier(int nb) {
     if (r > 2) return 0; 
     else return 1; // on a seulement 1 et nb comme diviseur donc premier
 }
-
-int my_system(char* chaine)
-{
-    char *argv[] = { "sh", "-c", chaine, NULL };
-    pid_t pid_fils;
-    int stat_fils;
-    switch (pid_fils = fork())
-    {
-    case (pid_t)-1:
-        perror("my_system/fork"); 
-        return EXIT_FAILURE;  
-        //break;
-
-    case (pid_t)0:
-        if (execv("/bin/sh", argv) == -1)
-        {
-           perror("my_system/execv");
-           return(EXIT_FAILURE);
-        }
-        
-        return EXIT_SUCCESS;  
-        //break;
-    
-    default:
-        wait(&stat_fils);
-        if(WIFEXITED(stat_fils) != 0 ){ 
-            printf("Fils:fin normale,code retour %d\n",WEXITSTATUS(stat_fils));
-        }
-        break;
-    }
-
-}
-
 
 void explorer(int debut, int fin) {
     int etat, pid, pid2;
@@ -66,13 +37,13 @@ void explorer(int debut, int fin) {
                 
                 if (pid2 == 0) { // on rentre dans le fils
                     
-                    char chaine[100];
-                    sprintf(chaine, "echo '%d  est un nombre premier écrit par le processus %d'>>nbr_premiers.txt", i, getpid());
+                    char chaine[500];
+                    sprintf(chaine, "echo '%d  est un nombre premier écrit par le processus %d qui a créé le processus sytem et qui a pour parent le processus %d. Le pid du processus qui a écrit le message sur le terminal est '$$>>nbr_premiers.txt", i, getpid(),getppid());
                     // sprintf stocke la chaîne formatée en mémoire dans un buffer pré-alloué nommée chaine.
                     // echo associé au >> (redirection) va permettre d'ajouter à la fin du fichier nbr_premier.txt la chaine de caractères entre single quote au lieu de l'afficher sur l'écran.
-                    my_system(chaine); // renvoie 0 si tout s'est bien passé, exécute la commande "chaine"
+                    system(chaine); // renvoie 0 si tout s'est bien passé, exécute la commande "chaine"
                     // c'est cette instruction qui va permettre l'écriture dans le fichier
-                    // printf("\n\nici le fils créateur du processus system, mon pid est %d et le pid de mon pere est %d\n\n", getpid(), getppid()); 
+                    //printf("\n\nici le fils créateur du processus system, mon pid est %d et le pid de mon pere est %d\n\n", getpid(), getppid()); 
 
                     sleep(2); // permet d'aller dans le père (l'instruction wait dans le père sera ainsi exécutée qui va entrainer un retour dans le fils)
                     exit(0); // terminaison normale du fils
